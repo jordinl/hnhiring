5ac448b42c60fc63359a0c481d6130669714e93d
Jordi Noguera <jordinoguera83@gmail.com>
2015-05-12 17:56:11 +0200

    use other user to find post

--- app/models/hacker_news.rb
+++ app/models/hacker_news.rb
@@ -21,7 +21,7 @@ class HackerNews
     end
 
     def find_hiring_post
-      html = HTTParty.get("https://news.ycombinator.com/submitted?id=whoishiring").body
+      html = HTTParty.get("https://news.ycombinator.com/submitted?id=_whoishiring").body
       link = Nokogiri::HTML.parse(html).css("td.title a").detect do |p|
         p.text.downcase =~ /^ask hn:.*hiring.*\(.*#{Time.now.strftime('%B').downcase[0,3]}.*#{Time.now.year}.*\)$/
       end
