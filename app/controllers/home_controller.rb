4b1f40774ec1458bbfe5ddb4d28e22cce3560dc5
Jordi Noguera <jordinoguera83+github@gmail.com>
2018-08-27 15:41:03 +0200

    Load comments separately

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def show
-    @month = Month.eager_load(:comments).order('comments.published_at ASC').last
+    @month = Month.last
+    @comments = @month.comments.order(published_at: :asc)
   end
 end
