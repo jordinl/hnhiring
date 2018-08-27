38a368e837e9521b08a9a476c36126c2abf21069
Jordi Noguera <jordinoguera83+github@gmail.com>
2018-08-27 17:31:40 +0200

    Sort descending

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def show
     @month = Month.last
-    @comments = @month.comments.order(published_at: :asc)
+    @comments = @month.comments.order(published_at: :desc)
   end
 end
