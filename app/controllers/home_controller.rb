3b798f8129d007e03aa4759abdf43ed78854f300
Jordi Noguera <jordinoguera83+github@gmail.com>
2018-09-07 17:31:59 +0200

    Sort months by number in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def show
-    @month = Month.last
+    @month = Month.order(:number).last
     @comments = @month.comments.order(published_at: :desc)
     @previous_month = @month.previous_month
   end
