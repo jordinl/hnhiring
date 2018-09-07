2c34da2965ea2fab938a71e2bb49dc3144b367bd
Jordi Noguera <jordinoguera83+github@gmail.com>
2018-09-07 16:50:23 +0200

    Extract Search Parser

--- app/models/hacker_news.rb
+++ app/models/hacker_news.rb
@@ -1,10 +1,9 @@
 class HackerNews
   class << self
-
     def get_last_hiring_post!
       month = Month.where(number: current_month).first
 
-      if month.nil? && post_id = find_hiring_post
+      if month.nil? && post_id = SearchParser.new.call
         month = Month.create(post_id: post_id, api_id: post_id, number: current_month)
       end
 
@@ -14,19 +13,6 @@ class HackerNews
     def current_month
       year, month = Time.now.year, Time.now.month
       year * 100 + month
-    end
-
-    def hiring_regex
-      /^ask hn:.*hiring.*\(.*#{Time.now.strftime('%B').downcase[0,3]}.*#{Time.now.year}.*\)$/
-    end
-
-    def find_hiring_post
-      html = HTTParty.get("https://news.ycombinator.com/submitted?id=whoishiring").body
-      link = Nokogiri::HTML.parse(html).css("td.title a").detect do |p|
-        p.text.downcase =~ /^ask hn:.*hiring.*\(.*#{Time.now.strftime('%B').downcase[0,3]}.*#{Time.now.year}.*\)$/
-      end
-
-      link.attr("href").split("=")[1] if link
     end
   end
 end
---
+++ app/parsers/search_parser.rb
@@ -0,0 +1,11 @@
+class SearchParser
+  def call
+    html = HTTParty.get("https://news.ycombinator.com/submitted?id=whoishiring").body
+    link = Nokogiri::HTML.parse(html).css("td.title a").detect do |p|
+      p.text.downcase =~ /^ask hn:.*hiring.*\(.*#{Time.now.strftime('%B').downcase[0,3]}.*#{Time.now.year}.*\)$/
+    end
+
+    link.attr("href").split("=")[1] if link
+  end
+
+end
