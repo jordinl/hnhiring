4db7f1aa81abb8f3fcec059053a436b89aeb86b3
Jordi Noguera <jordinoguera83+github@gmail.com>
2018-09-07 17:15:37 +0200

    Move load comments from month to import month service

--- app/models/month.rb
+++ app/models/month.rb
@@ -1,39 +1,27 @@
 class Month < ActiveRecord::Base
-  include ActionView::Helpers::TextHelper
-  include ActionView::Helpers::SanitizeHelper
-
   validates :number, :api_id, presence: true
   validates :number, uniqueness: true
 
   has_many :comments, dependent: :destroy
 
   def url
-    "https://news.ycombinator.com/item?id=#{api_id}"
+    return unless api_id
+    @url ||= "https://news.ycombinator.com/item?id=#{api_id}"
+  end
+
+  def name
+    self.name = Date.new(year, month).strftime("%B")
   end
 
   def previous_month
     Month.order(number: :desc).where('number < ?', number).first
   end
 
-  def number=(number)
-    month, year = number % 100, number / 100
-    self.name = Date.new(year, month).strftime("%B")
-    super
+  def month
+    number % 100
   end
 
   def year
     number / 100
-  end
-
-  def load_comments
-    comments.destroy_all
-
-    html_comments = CommentsParser.new(url).call
-
-    html_comments.each do |comment|
-      published_at = Chronic.parse(comment['date'])
-      description = Rinku.auto_link(simple_format(sanitize(comment['text'], tags: %w(strong em a p))))
-      comments.create!(description: description, username: comment['username'], published_at: published_at)
-    end
   end
 end
--- app/services/import_month_service.rb
+++ app/services/import_month_service.rb
@@ -1,4 +1,7 @@
 class ImportMonthService
+  include ActionView::Helpers::TextHelper
+  include ActionView::Helpers::SanitizeHelper
+
   def initialize(number: nil, api_id: nil)
     @number = number || current_month
     @api_id = api_id || SearchParser.new.call
@@ -6,7 +9,17 @@ class ImportMonthService
 
   def call
     month = Month.find_or_create_by!(number: @number, api_id: @api_id)
-    month.load_comments if month.present?
+    return unless month
+
+    month.comments.destroy_all
+
+    html_comments = CommentsParser.new(month.url).call
+
+    html_comments.each do |comment|
+      published_at = Chronic.parse(comment['date'])
+      description = format(comment['text'])
+      month.comments.create!(description: description, username: comment['username'], published_at: published_at)
+    end
   end
 
   private
@@ -14,5 +27,9 @@ class ImportMonthService
   def current_month
     year, month = Time.now.year, Time.now.month
     year * 100 + month
+  end
+
+  def format(comment)
+    Rinku.auto_link(simple_format(sanitize(comment, tags: %w(strong em a p))))
   end
 end
