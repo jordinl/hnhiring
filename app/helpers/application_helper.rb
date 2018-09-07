51cbcea87c731b9f9470bec78614c13530a8c0a2
Jordi Noguera <jordinoguera83+github@gmail.com>
2018-09-07 18:45:24 +0200

    Show time ago only if < 24 hours ago

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def default_title
     "Compilation of Hacker News 'Who is Hiring?' Posts"
   end
+
+  def comment_timestamp(timestamp)
+    timestamp < 24.hours.ago ? timestamp.to_date : time_ago_in_words(timestamp)
+  end
 end
