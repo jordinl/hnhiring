828e4ac9735890c0e48554646498c78b817421ee
Jordi Noguera <jordinoguera83+github@gmail.com>
2018-09-08 09:37:13 +0200

    Remove limit 50 in months controller

--- app/controllers/months_controller.rb
+++ app/controllers/months_controller.rb
@@ -1,7 +1,7 @@
 class MonthsController < ApplicationController
   def show
     @month = Month.find_by_number(params[:id])
-    @comments = @month.comments.order(published_at: :desc).limit(50)
+    @comments = @month.comments.order(published_at: :desc)
     @previous_month = @month.previous_month
   end
 end
