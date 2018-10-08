f06fbc6206ed66d1eb050c925ffc474699262a6c
Jordi Noguera <jordinoguera83+github@gmail.com>
2018-10-08 14:35:32 +0200

    Remove loading comments asynchronously

--- app/assets/javascripts/loadComments.js
+++
@@ -1,6 +0,0 @@
-$(document).on('turbolinks:load', function () {
-  $('[data-load-comment]').each(function() {
-    var $comment = $(this);
-    setTimeout(function () { $comment.removeClass('hidden') }, 20)
-  })
-})
