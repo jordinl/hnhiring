adf24844c24a31a168026b12f6b2a76b2cb7c192
Jordi Noguera <jordinoguera83+github@gmail.com>
2018-10-08 13:20:14 +0200

    Extract DescriptionFormatter and fix missing paragraphs

--- app/services/import_month_service.rb
+++ app/services/import_month_service.rb
@@ -1,7 +1,4 @@
 class ImportMonthService
-  include ActionView::Helpers::TextHelper
-  include ActionView::Helpers::SanitizeHelper
-
   def initialize(number: nil, api_id: nil)
     @number = number || current_month
     @api_id = api_id || SearchParser.new.call
@@ -17,7 +14,7 @@ class ImportMonthService
       comment = month.comments.find_or_initialize_by(api_id: html_comment['id'])
       comment.published_at ||= Chronic.parse(html_comment['date'])
       comment.username = html_comment['username']
-      comment.description = format(html_comment['text'])
+      comment.description = DescriptionFormatter.call(html_comment['text'])
       comment.save! if comment.changed?
     end
   end
@@ -27,9 +24,5 @@ class ImportMonthService
   def current_month
     year, month = Time.now.year, Time.now.month
     year * 100 + month
-  end
-
-  def format(comment)
-    Rinku.auto_link(sanitize(comment, tags: %w(strong em a p)))
   end
 end
