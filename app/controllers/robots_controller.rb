f8e353c3374736cb8ed8b99429959e6536acc3ed
Jordi Noguera <jordinoguera83+github@gmail.com>
2018-10-09 16:02:02 +0200

    Flix conditional to show robots.txt on production

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -2,6 +2,6 @@ class RobotsController < ApplicationController
   layout false
 
   def show
-    render 'robots/disallow_all' if Rails.env.production?
+    render 'robots/disallow_all' unless Rails.env.production?
   end
 end
