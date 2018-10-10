330cdcb9717abbd0f669ee2c5587286c2a054ab3
Jordi Noguera <jordinoguera83+github@gmail.com>
2018-10-10 09:42:00 +0200

    Prettify search URL

--- app/assets/javascripts/submit.js
+++ app/assets/javascripts/submit.js
@@ -1,15 +1,19 @@
 $(document).on('turbolinks:load', function () {
-  $('[data-submit]').each(function() {
+  $('[data-submit]').each(function () {
     var $link = $(this)
     $link.on('click', function (e) {
-      e.preventDefault();
-      var $form = $($link.data('submit'));
-      var url = $form.attr('action');
-      var query = $form.serialize()
+      e.preventDefault()
+      var $form = $($link.data('submit'))
+      var url = $form.attr('action')
+      var query = $form.serializeArray().filter(function (input) {
+        return input.value
+      }).map(function(input) {
+        return input.name + '=' + input.value
+      }).join('&').replace(/\s+/g, '+')
       if (query) {
         url += '?' + query
       }
-      Turbolinks.visit(url);
-    });
-  });
+      Turbolinks.visit(url)
+    })
+  })
 })
