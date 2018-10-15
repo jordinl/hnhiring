d96e94406f75f8a4f2f6661e82a3dee71e81ca06
Jordi Noguera <jordinoguera83+github@gmail.com>
2018-10-15 19:09:49 +0200

    Index Node

--- app/models/technology.rb
+++ app/models/technology.rb
@@ -17,6 +17,7 @@ class Technology < ActiveRecord::Base
     java:           -> { Job.matching_words(%w(java)) },
     javascript:     -> { Job.matching_words(%w(javascript)) },
     kotlin:         -> { Job.matching_words(%w(kotlin)) },
+    node:           -> { Job.matching_words(%w(node nodejs)) },
     php:            -> { Job.matching_words(%w(php)) },
     python:         -> { Job.matching_words(%w(python django)) },
     rails:          -> { Job.matching_words(%w(rails)) },
