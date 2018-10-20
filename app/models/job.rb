4a2fa88398523cd2b38ef6d4f4507811ca304d9e
Jordi Noguera <jordinoguera83+github@gmail.com>
2018-10-20 10:52:03 +0200

    Add bunch more cities

--- app/models/job.rb
+++ app/models/job.rb
@@ -7,11 +7,20 @@ class Job < ActiveRecord::Base
 
   scope :matching_text_sensitive, -> (text) { where('description ~ ?', text) }
   scope :matching_text, -> (text) { where('description ~* ?', text) }
+  scope :not_matching_text, -> (text) { where('description !~* ?', text) }
   scope :matching_words, -> (words) do
     scope = nil
     words.each do |word|
       word_scope = matching_text('\y' + word + '\y')
       scope = scope ? scope.or(word_scope) : word_scope
+    end
+    scope
+  end
+  scope :not_matching_words, -> (words) do
+    scope = nil
+    words.each do |word|
+      word_scope = not_matching_text('\y' + word + '\y')
+      scope = scope ? scope.merge(word_scope) : word_scope
     end
     scope
   end
