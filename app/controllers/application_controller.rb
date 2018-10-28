4f49a69dbba6ede9d51f2575f4479aedec98ae9d
Jordi Noguera <jordinoguera83+github@gmail.com>
2018-10-28 09:26:19 +0100

    Add menu modal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :technology_keywords, :location_keywords, :technologies, :locations
+  helper_method :technology_keywords, :location_keywords, :technologies, :locations, :last_month
 
   private
 
@@ -35,5 +35,9 @@ class ApplicationController < ActionController::Base
 
   def locations
     @locations ||= Keyword.location.all
+  end
+
+  def last_month
+    @last_month ||= Month.order(:number).last
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def show
-    @last_month = Month.order(:number).last
     @first_month = Month.order(:number).first
     @jobs_count = Job.count
     @next_post_time = NextHiringPost.call
