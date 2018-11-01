2ef1c6b26a541509270c03685a9ad1140da8d125
Jordi Noguera <jordinoguera83+github@gmail.com>
2018-11-01 20:35:49 +0100

    Fix next post publish time calculation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   def show
     @first_month = Month.order(:number).first
     @jobs_count = Job.count
-    @next_post_time = NextHiringPost.call
+    @next_post_time = Time.zone.today.next_month.at_beginning_of_month.in_time_zone + 11 * 3_600
   end
 end
