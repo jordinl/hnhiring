43788e597382854303a906ee117403e0f9b69ae9
Jordi Noguera <jordinoguera83+github@gmail.com>
2018-12-02 08:54:50 +0100

    Hardcode next date for now

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   def show
     @first_month = Month.order(:number).first
     @jobs_count = Job.count
-    @next_post_time = Time.zone.today.next_month.at_beginning_of_month.in_time_zone + 11 * 3_600
+    @next_post_time = Date.new(2018, 12, 3).in_time_zone + 11 * 3_600
   end
 end
