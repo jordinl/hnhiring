fbfe059b86b22f1f31ad323f8b9b81e4cd960e54
Jordi Noguera <jordinoguera83+github@gmail.com>
2019-01-02 20:04:38 +0100

    Update next post date

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   def show
     @first_month = Month.order(:number).first
     @jobs_count = Job.count
-    @next_post_time = Date.new(2019, 1, 1).in_time_zone + 11 * 3_600
+    @next_post_time = Date.new(2019, 2, 1).in_time_zone + 11 * 3_600
   end
 end
