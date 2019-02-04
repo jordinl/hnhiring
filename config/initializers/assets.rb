476d4880bdb3e002df7fb7daa0c3f9e5a9e1b441
Jordi Noguera <jordinoguera83+github@gmail.com>
2019-02-04 10:28:59 +0100

    Font awesome part 3

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css
 #
-Rails.application.config.assets.precompile += %w(fontawesome/*)
+Rails.application.config.assets.precompile += [/fontawesome\/.*\.(?:css|svg|eot|woff|ttf)\z/]
