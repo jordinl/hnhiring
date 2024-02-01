75a7f3bbd628f2868c7974a2f9649071b355f865
Jordi Noguera <jordinoguera83@gmail.com>
2024-02-01 16:37:00 +0100

    Add countdown to homepage

--- app/assets/javascripts/formatDatetime.js
+++ app/assets/javascripts/formatDatetime.js
@@ -1,9 +1,26 @@
 $(document).on('turbolinks:load', function () {
   $('[data-format-datetime]').each(function () {
     var $el = $(this);
-    var date = new Date($el.data('format-datetime'))
-    var dateString = date.toLocaleString('en-us', { month: 'long', day: 'numeric' })
-    var timeString = date.toLocaleString('en-us', { timeStyle: 'short' })
-    $el.text(dateString + ' at ' + timeString)
+
+    function setCountdown() {
+      var difference = Math.floor(($el.data('format-datetime') - new Date().getTime()) / 1000)
+      var seconds = difference % 60
+      difference = Math.floor(difference / 60)
+      var minutes = difference % 60
+      difference = Math.floor(difference / 60)
+      var hours = difference % 24
+      var days = Math.floor(difference / 24)
+      var text = [
+        days > 0 ? `${days}d` : null,
+        hours > 0 || days > 0 ? `${hours}h` : null,
+        minutes > 0 || days > 0 || hours > 0 ? `${minutes}m` : null,
+        `${seconds}s`
+      ].filter(_ => _).join(' ')
+      $el.text(text)
+    }
+
+    setCountdown()
+
+    setInterval(setCountdown, 1000)
   })
 })
