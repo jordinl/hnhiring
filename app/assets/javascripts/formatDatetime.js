9d6decd3e187539cf5a2827cce9485045be58d8a
Jordi Noguera <jordinoguera83@gmail.com>
2024-02-01 14:11:29 +0100

    Tweaks to time presentation

--- app/assets/javascripts/formatDatetime.js
+++ app/assets/javascripts/formatDatetime.js
@@ -2,8 +2,8 @@ $(document).on('turbolinks:load', function () {
   $('[data-format-datetime]').each(function () {
     var $el = $(this);
     var date = new Date($el.data('format-datetime'))
-    var dateString = date.toLocaleString('en-us', { year: 'numeric', month: 'long', day: 'numeric' })
-    var timeString = date.toLocaleString('en-us', { hour: 'numeric' })
+    var dateString = date.toLocaleString('en-us', { month: 'long', day: 'numeric' })
+    var timeString = date.toLocaleString('en-us', { timeStyle: 'short' })
     $el.text(dateString + ' at ' + timeString)
   })
 })
