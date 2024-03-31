3e0fbab44f2ff6a2b3293a2e32e7cb365a712a89
Jordi Noguera <jordinoguera83@gmail.com>
2024-03-31 08:57:22 +0200

    Fix showing seconds in countdown

--- app/assets/javascripts/formatDatetime.js
+++ app/assets/javascripts/formatDatetime.js
@@ -5,11 +5,11 @@ $(document).on('turbolinks:load', function () {
     function setCountdown() {
       var difference = Math.floor(($el.data('format-datetime') - new Date().getTime()) / 1000)
       var seconds = difference % 60
-      difference = Math.floor(difference / 60)
-      var minutes = difference % 60
-      difference = Math.floor(difference / 60)
-      var hours = difference % 24
-      var days = Math.floor(difference / 24)
+      var rest = Math.floor(difference / 60)
+      var minutes = rest % 60
+      rest = Math.floor(rest / 60)
+      var hours = rest % 24
+      var days = Math.floor(rest / 24)
       var text = [
         days > 0 ? `${days}d` : null,
         hours > 0 || days > 0 ? `${hours}h` : null,
